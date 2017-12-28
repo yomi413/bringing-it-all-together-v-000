cb4cf9bd5ad4cdfe49a7f28a1069f0e219b11d42
Yomaira Escano <yjescano@gmail.com>
2017-12-28 02:12:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,7 +41,7 @@ class Dog
 
       DB[:conn].execute(sql, self.name, self.breed)
 
-      @id = DB[:conn].execute("SELECT id FROM dogs")[0]
+      @id = DB[:conn].execute("SELECT id FROM dogs")[0][-1]
     end
     self
     # binding.pry
